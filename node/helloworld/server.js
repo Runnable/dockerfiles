239f8774186875e1ba8f8305e0dc119781fd2952
Praful Rana <prafulrana@gmail.com>
2013-06-20 20:42:08 -0600

    Update server.js

--- node/helloworld/server.js
+++ node/helloworld/server.js
@@ -1,6 +1,11 @@
 var http = require('http');
+var redis = require("redis").createClient();
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  redis.get("foo", function (err, data) {
+    res.end(data);
+  });
 }).listen(80);
 console.log('Hello Kitty');
+redis.set("foo", "bar");
